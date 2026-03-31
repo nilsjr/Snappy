ab3cc9cfef094a9c3ffe89bcb7942437b516a430
Nils Druyen <info@nilsdruyen.de>
2026-03-31 19:40:41 +0200

    Feature/agp 9.0 (#351)
    * fix(deps): update androidGradle to v9.0.0
    
    This commit updates the `androidGradle` version to `9.0.0` in `libs.versions.toml` and adds several Android-related configuration flags to `gradle.properties` to maintain compatibility and define build behavior for the new version.
    
    * fix(deps): update androidGradle to v9.0.0
    
    This commit updates the `androidGradle` version to `9.0.0` in `libs.versions.toml` and adds several Android-related configuration flags to `gradle.properties` to maintain compatibility and define build behavior for the new version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
-  alias(libs.plugins.kotlin.android) apply false
+//  alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.kotlin.dokka) apply false
   alias(libs.plugins.kotlin.parcelize) apply false
